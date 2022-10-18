e96aa2a5ce5945bd16dd6de9bc0f34036881b0d1
nicomuros <nicomuros@gmail.com>
2022-10-17 21:32:21 -0300

    Avances en trabajo final

--- TrabajoFinalPrimerAño/src/trabajofinalprimeraño/TrabajoFinalPrimerAño.java
+++ TrabajoFinalPrimerAño/src/trabajofinalprimeraño/TrabajoFinalPrimerAño.java
@@ -5,26 +5,57 @@ import javax.swing.JOptionPane;
 public class TrabajoFinalPrimerAño {  
     
     public static void main(String[] args) {
+        /* En esta parte se crean los objetos usuarios, a partir de la clase
+        homonima que se encuentra en el paquete. En la clase usuarios tenemos
+        los atributos "nombre" y "puntaje", junto con algunos métodos que muestran
+        nombres, puntajes, y permiten la modificación de los últimos.
+        */
         Usuarios usuario1 = new Usuarios();
         Usuarios usuario2 = new Usuarios();
-        byte eleccion;
+        
+        /* Usamos la función cargarUsuarios() para cargar los datos a los objetos*/
         cargarUsuarios(usuario1,usuario2);
         
-        while (true){
+        /*ciclo infinito que muestra el menú la primera vez, y cada vez que el usuario
+        salga de un juego. El ciclo se rompe cuando el usuario ingresa un "0" (cero). Para
+        eso usamos la funcion mostrarMenu().
+        */
+        byte eleccion;
+        while (true){ 
+            /*Enviamos los objetos usuario1 y usuario2 a la función mostrarMenu(), la cual
+            devuelve un byte con el valor ingresado por el usuario.
+            */
             eleccion = mostrarMenu(usuario1,usuario2);
-            var continuar = true;
+            var continuar = true; //variable que se encarga de mantener el ciclo funcionando
             switch (eleccion){
                 case 1: 
-                    usuario1.sumarPuntaje();
+                    usuario1.sumarPuntaje(); //mediante este método sumamos 1 punto al usuario1
                     break;
                 case 2:
-                    usuario2.sumarPuntaje();
+                    usuario2.sumarPuntaje(); //mediante este método sumamos 1 punto al usuario2
                     break;
                 case 3:
+                    //si a usuario1.sumarPuntaje le ingresamos como argumento un valor númerico, ese puntaje se va a sumar
                     usuario1.sumarPuntaje(Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar puntaje a sumar: ")));
                     break;
                 case 4:
                     usuario2.sumarPuntaje(Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar puntaje a sumar: ")));
+                    break;
+                case 5:
+                    /* como es un juego singleplayer, se muestra un pequeño menú que capta que 
+                    usuario va a jugar, para enviarselo al juego (que se encuentra en la clase Juegos)
+                    */
+                    var jugador = Integer.parseInt(JOptionPane.showInputDialog(
+                            "¿Quien va a jugar?\n"
+                            + "1. " + usuario1.nombre + "\n"
+                            + "2. " + usuario2.nombre + "\n"));
+                    if (jugador == 1) { 
+                        Juegos.iowa(usuario1); //de esta forma se ejecuta el juego que está en la clase Juegos, se envía
+                        //el objeto usuario1 para que trabaje con él
+                    } else {
+                        Juegos.iowa(usuario2);
+                    }
+                    
                     break;
                 case 0:
                     JOptionPane.showMessageDialog(null, "Muchas gracias por jugar!!");
@@ -36,6 +67,8 @@ public class TrabajoFinalPrimerAño {
         }
     }
     
+    /* Funcion que se encarga de mostrar en pantalla la interfaz donde se ingresan los usuarios */
+    /* es void porque no retorna nada */
     public static void cargarUsuarios( Usuarios usuario1 , Usuarios usuario2 ){
         String nombreUsuario;
         nombreUsuario = JOptionPane.showInputDialog(null,
@@ -51,8 +84,12 @@ public class TrabajoFinalPrimerAño {
         usuario2.crearUsuario(nombreUsuario);
     }
     
+    /* funcion que muestra en pantalla el menú. es byte porque retorna valores entre -128 y 127
+    de esta forma optimizamos el uso de la memoria.
+    */
     public static byte mostrarMenu( Usuarios usuario1 , Usuarios usuario2 ){
         while (true){
+            /* Para mostrar un menú con muchas lineas, se agrega \n al final de cada una. */
             var eleccion = Byte.parseByte(JOptionPane.showInputDialog(null, 
                 "MENU PRINCIPAL\n"
                 +usuario1.nombre + ": " + usuario1.puntaje + "\n"
@@ -60,13 +97,13 @@ public class TrabajoFinalPrimerAño {
                 +"1. Sumar un punto a " + usuario1.nombre + "\n"
                 +"2. Sumar un punto a " + usuario2.nombre + "\n"
                 +"3. Sumar varios puntos a " + usuario1.nombre + "\n"
-                +"4. Sumar varios puntos a " + usuario2.nombre + "\n\n"
+                +"4. Sumar varios puntos a " + usuario2.nombre + "\n"
+                +"5. IOWA\n\n"
                 +"0. Salir"));
             
-            if (eleccion >= 0 && eleccion <= 4){
+            if (eleccion >= 0 && eleccion <= 5){
                 return eleccion;
             }
         }
-    }
-    
+    }  
 }
--- TrabajoFinalPrimerAño/src/trabajofinalprimeraño/Usuarios.java
+++ TrabajoFinalPrimerAño/src/trabajofinalprimeraño/Usuarios.java
@@ -1,28 +1,19 @@
 package trabajofinalprimeraño;
 public class Usuarios{
     
-        public String nombre;
-        public Integer puntaje;
+        public String nombre; //Atributo donde se almacenará el nombre del objeto usuario
+        public Integer puntaje; //Atributo donde se almacenará el puntaje del objeto usuario
         
-        public void crearUsuario(String args){
+        public void crearUsuario(String args){ //Método para asignar el nombre del usuario
             nombre = args;
-            puntaje = 0;
+            puntaje = 0; //Puntaje inicial
         }
-        
-        public String mostrarNombre()
-        {
-            return nombre;
-        }
-        public int mostrarPuntaje()
-        {
-            return puntaje;
-        }
-        
-        public void sumarPuntaje()
+              
+        public void sumarPuntaje() //Método que suma 1 punto
         {
             puntaje++;
         }
-        public void sumarPuntaje(int puntos)
+        public void sumarPuntaje(int puntos) //Método que suma n puntos
         {
             puntaje += puntos;
         }
